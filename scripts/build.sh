04157f238b700d29bd9c46def7e9f87f01d93578
XiaotaoChen <chenxiaotao1234@gmail.com>
2026-04-01 09:57:43 +0000

    add build script

---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# pip wheel . --no-build-isolation -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+# uv pip install . --no-build-isolation -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+uv pip install -e . --no-build-isolation -i https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
